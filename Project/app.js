003362f66740dfe270c787854774023b258f96ed
Roy97 <s224021028@deakin.edu.au>
2024-08-25 17:58:20 +1000

    initial commit

--- Project/app.js
+++ Project/app.js
@@ -1,3 +1,10 @@
 var txt1 = "What a very";
 var txt2 = "nice day";
 document.getElementById("demo").innerHTML = txt1 + txt2;
+
+function toCelsius(f) {
+    return (5/9) * (f-32);
+  }
+  
+  let value = toCelsius(77);
+  document.getElementById("demo").innerHTML = value;
